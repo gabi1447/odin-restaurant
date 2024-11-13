05a2d0e7ee8ac63f16aeaec46a979b7101512f48
Gabriel <gabrielpachecotec@gmail.com>
2024-11-13 11:44:42 +0000

    Add generateLogoHeadline() to generateHomepage()
    This function creates a div container for both the logo and
    the headline of the restaurant.

--- src/homepage.js
+++ src/homepage.js
@@ -3,12 +3,35 @@ import logo from './burger-logo.jpg';
 export function generateHomepage() {
     const content = document.getElementById('content');
     
-    const headlineText = "Bite into Happiness!";
     const descriptionText = "At Pacheco's, we believe every bite should be unforgettable. Our burgers are crafted with the freshest ingredients, juicy patties, and a perfect blend of flavors that’ll leave you craving more. Whether you’re a fan of classic cheeseburgers or adventurous flavors, each burger is made to order with love and care. Come experience the joy of a truly delicious burger and let us make your meal the highlight of your day!"
     
     // Heading
     const heading = generateHeading();
     content.appendChild(heading);
+
+    // Logo Headline
+    const logoHeadline = generateLogoHeadline();
+    content.appendChild(logoHeadline);
+
+    // TextDescription
+    const burgerText = document.createElement("p");
+    burgerText.classList.add('burgerText');
+    burgerText.innerText = descriptionText;
+
+    content.appendChild(burgerText);
+}
+
+function generateHeading() {
+    const restaurantName = "Pacheco's";
+
+    // Heading
+    const burgerHeading = document.createElement('h1');
+    burgerHeading.innerText = restaurantName;
+    return burgerHeading;
+}
+
+function generateLogoHeadline() {
+    const headlineText = "Bite into Happiness!";
 
     // LogoHeadline
     const logoHeadline = document.createElement('div');
@@ -25,25 +48,5 @@ export function generateHomepage() {
     headline.innerText = headlineText;
     logoHeadline.appendChild(headline);
 
-    content.appendChild(logoHeadline);
-
-    // TextDescription
-    const burgerText = document.createElement("p");
-    burgerText.classList.add('burgerText');
-    burgerText.innerText = descriptionText;
-
-    content.appendChild(burgerText);
-}
-
-export function testing() {
-    console.log('testing');
-}
-
-function generateHeading() {
-    const restaurantName = "Pacheco's";
-
-    // Heading
-    const burgerHeading = document.createElement('h1');
-    burgerHeading.innerText = restaurantName;
-    return burgerHeading;
+    return logoHeadline;
 }
