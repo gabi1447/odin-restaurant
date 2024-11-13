f86663b1d8adcb757b5b61821d30386e09b1ed62
Gabriel <gabrielpachecotec@gmail.com>
2024-11-13 11:55:24 +0000

    Add generateDescription() to generateHomepage()

--- src/homepage.js
+++ src/homepage.js
@@ -2,9 +2,7 @@ import logo from './burger-logo.jpg';
 
 export function generateHomepage() {
     const content = document.getElementById('content');
-    
-    const descriptionText = "At Pacheco's, we believe every bite should be unforgettable. Our burgers are crafted with the freshest ingredients, juicy patties, and a perfect blend of flavors that’ll leave you craving more. Whether you’re a fan of classic cheeseburgers or adventurous flavors, each burger is made to order with love and care. Come experience the joy of a truly delicious burger and let us make your meal the highlight of your day!"
-    
+        
     // Heading
     const heading = generateHeading();
     content.appendChild(heading);
@@ -13,12 +11,9 @@ export function generateHomepage() {
     const logoHeadline = generateLogoHeadline();
     content.appendChild(logoHeadline);
 
-    // TextDescription
-    const burgerText = document.createElement("p");
-    burgerText.classList.add('burgerText');
-    burgerText.innerText = descriptionText;
-
-    content.appendChild(burgerText);
+    // Restaurant Description
+    const restaurantDescription = generateDescription();
+    content.appendChild(restaurantDescription);
 }
 
 function generateHeading() {
@@ -49,4 +44,14 @@ function generateLogoHeadline() {
     logoHeadline.appendChild(headline);
 
     return logoHeadline;
+}
+
+function generateDescription() {
+    const description = "At Pacheco's, we believe every bite should be unforgettable. Our burgers are crafted with the freshest ingredients, juicy patties, and a perfect blend of flavors that’ll leave you craving more. Whether you’re a fan of classic cheeseburgers or adventurous flavors, each burger is made to order with love and care. Come experience the joy of a truly delicious burger and let us make your meal the highlight of your day!"
+
+    const restaurantDescription = document.createElement("p");
+    restaurantDescription.classList.add('description');
+    restaurantDescription.innerText = description;
+
+    return restaurantDescription;
 }
